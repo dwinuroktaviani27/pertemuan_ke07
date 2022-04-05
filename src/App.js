4b9febac9663779939dd1f3c8b3ccf73ab2b2b4b
dwinuroktaviani27 <1941720239@student.polinema.ac.id>
2022-04-06 06:03:00 +0700

    Pertemuan_07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Link,Routes, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoaderDemo from './LoaderDemo/LoaderDemo.jsx';
+import RequireAuthDemo from './ProtectedRoutes/RequireAuthDemo.jsx';
+import RefsDemo from './RefsDemo/RefsDemo.jsx';
+import GenericContainerDemo from './GenericContainer/GenericContainerDemo.jsx';
+import CustomFormDemo from './CustomForm/CustomFormDemo';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated : false,
+    }
+  }
+  render() {
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark ">
+        
+           <ul className="navbar-nav mr-auto">
+            <li className="nav-item active"><Link to="/">Home</Link></li>
+            <li><Link to="/loaderdemo">Loader Demo</Link></li>
+            <li><Link to="/genericcontainer">Generic Container/State abstraction</Link></li>
+            <li><Link to="/customform">CustomForm</Link></li>
+            <li><Link to="/refsdemo">Refs Demo</Link></li>
+             <li><Link to="/requireauth">Protected Routes Demo</Link></li>
+          </ul>
+         
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home}/>
+
+          
+           <Route path="/loaderdemo" component={LoaderDemo}/>
+           <Route path="/genericcontainer" component={GenericContainerDemo}/>
+           <Route path="/customform" component={CustomFormDemo}/>
+           <Route path="/refsdemo" component={RefsDemo}/>
+           <Route path="/requireauth" component = {RequireAuthDemo} />
+
+        </Switch>
+
+
+      
+      </div>
+
+      
+    );
+  }
 }
 
+const Home = () => {
+  return(
+    <div> You are at Home </div>)
+}
 export default App;
